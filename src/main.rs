9de0036ca066851154495d269bf432caa5c9bd15
Kirk Patrick <kirkgo@gmail.com>
2023-09-04 00:39:02 +0100

    Import libraries and create Contact model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use postgres::{ Client, NoTls};
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream};
+use std::io::{ Read, Write};
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+// Model: Contact struct with id, name, email and phone
+#[derive(Serialize, Deserialize)]
+struct Contact {
+    id: Option<i32>,
+    name: String,
+    email: String,
+    phone: String,
 }
